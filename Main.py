c25d85797df3a7636020a020f9f408bcc3b3cf07
Srimansrisanth <167223151+Srimansrisanth@users.noreply.github.com>
2024-04-16 21:41:44 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,11 +2,9 @@ print('----WELCOME TO MONEY BOT----')
 name=input('ENTER YOUR NAME:')
 ID=int(input('ENTER YOUR NAME :'))
 invite_code=int(input('ENTER YOUR INVITE CODE :'))
-if ID == '0987654321':
-  print('ok!!!!')
-  print('you are done!!')  
-else ID =='0987654322':
-  print('ok!!!!')
+if ID == [0987654321,0987654322,0987654323,0987654324,0987654325]:
+  print('login sucussful !!')
+
 
 
 
