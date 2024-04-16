5b0ce1e7a5a6a991570bbed5ac2ca3a9bd1e0094
Srimansrisanth <167223151+Srimansrisanth@users.noreply.github.com>
2024-04-16 20:51:03 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+print('----WELCOME TO MONEY BOT----')
+name=input('ENTER YOUR NAME:')
+ID=int(input('ENTER YOUR NAME :'))
+invite_code=int(input('ENTER YOUR INVITE CODE :'))
+if ID == '0987654321':
+  print('ok!!!!')
+  print('you are done!!')  
+else ID =='0987654322':
+  print('ok!!!!')
+
+
+
